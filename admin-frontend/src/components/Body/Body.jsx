1b1c36ce7a4072ef4b16a6da796e29517316473e
oyebode amirdeen <oyebodeamirdeen@gmail.com>
2023-01-10 12:12:23 +0100

    working on media queries

--- admin-frontend/src/components/Body/Body.jsx
+++ admin-frontend/src/components/Body/Body.jsx
@@ -3,10 +3,16 @@ import './body.scss'
 import adminUser from '../../assets/images/admin-user.jpeg'
 
 const Header = ({children}) => {
+    const showMenu = () => {
+        const menubar = document.getElementById('menu');
+        menubar.classList.toggle('shown');
+    }
+    
   return (
     <section id='interface'>
         <div className='navigation'>
-            <div className='nl'>
+            <div className='n1'>
+                <div><i className="fas fa-bars" id="menu-btn" onClick={showMenu}></i></div>
                 <div className='search'>
                     <i className='far fa-search'></i>
                     <input type='text' placeholder='Enter Order ID' />
